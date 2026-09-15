ed3940a479bd0f2c77b2006729a0c8254b7f6433
^_𝔸ssem <126397591+Assem44@users.noreply.github.com>
2026-09-15 17:45:34 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -6,7 +6,7 @@ import subprocess
 import os
 from pathlib import Path
 from config import (
-DOWNLOAD_DIR, DOWNLOAD_QUALITY, SEND_VIDEO_TO_TELEGERAM
+DOWNLOAD_DIR, DOWNLOAD_QUALITY, SEND_VIDEO_TO_TELEGRAM
 )
 import logging
 from notifier import send_telegram
